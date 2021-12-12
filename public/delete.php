5b1173401d6de740d56eb63ea809b179de3c1b58
Scott Verhage <84455759+severhage@users.noreply.github.com>
2021-12-12 20:26:48 +0100

    sluithaakjes toegevoegd (om errors te voorkomen)

--- public/delete.php
+++ public/delete.php
@@ -1,5 +1,5 @@
 <?php
-    include_once '../header.php'
+    include_once '../header.php';
 ?>
 
 <center>
@@ -13,5 +13,5 @@
     </div>
 </center>
 <?php
-    include_once '../footer.php'
+    include_once '../footer.php';
 ?>
--- public/public/index.php
+++ public/public/index.php
@@ -1,3 +1,3 @@
 <?php 
-	include_once '../../redirect.php'
+	include_once '../../redirect.php';
  ?>
